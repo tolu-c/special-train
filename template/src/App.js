8c0daef4e3d8f04c55a8d481866b88b85c3e3aca
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-02-11 22:28:42 +0100

    basic template for both react and django

---
+++ template/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-4xl text-blue-600 font-bold bg-blue-50">
+      hello world
+    </div>
+  );
+}
+
+export default App;
